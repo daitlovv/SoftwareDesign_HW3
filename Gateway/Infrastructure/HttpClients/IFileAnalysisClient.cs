2a428bd2a007f69f60ae3898bbd5073381a37fb6
Дёшина Кристина Валентиновна <daitlovvv@gmail.com>
2025-12-13 23:32:51 +0300

    Delete Gateway directory

--- Gateway/Infrastructure/HttpClients/IFileAnalysisClient.cs
+++
@@ -1,9 +0,0 @@
-using Gateway.Application.DTOs;
-
-namespace Gateway.Infrastructure.HttpClients;
-
-public interface IFileAnalysisClient
-{
-    Task<ReportDto> AnalyzeFileAsync(Guid fileId, string workId, string studentName);
-    Task<IEnumerable<ReportDto>> GetReportsAsync(string workId);
-}
--- Gateway/Infrastructure/HttpClients/IFileStorageClient.cs
+++
@@ -1,7 +0,0 @@
-namespace Gateway.Infrastructure.HttpClients;
-
-public interface IFileStorageClient
-{
-    Task<Guid> UploadFileAsync(IFormFile file);
-    Task<string> GetFileTextAsync(Guid fileId);
-}
