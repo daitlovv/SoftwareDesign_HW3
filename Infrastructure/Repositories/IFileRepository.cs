50ef8123daa263316a451f6d5c1e4fca35b61b1e
Дёшина Кристина Валентиновна <daitlovvv@gmail.com>
2025-12-13 23:56:00 +0300

    Delete Infrastructure directory

--- Infrastructure/Repositories/IFileRepository.cs
+++
@@ -1,9 +0,0 @@
-using FileStorage.Domain.Entities;
-
-namespace FileStorage.Infrastructure.Repositories;
-
-public interface IFileRepository
-{
-    Task AddAsync(StoredFile entity);
-    Task<StoredFile?> GetAsync(Guid fileId);
-}
